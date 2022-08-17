57fa0b24d69687f791b2b9625c9b11fa5ed95c38
saib ali <saibniazi7@gmail.com>
2022-08-17 18:27:54 +0500

    chnages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,34 +17,74 @@
         input_data: '',
         place_data: 'Enter keyword',
         firstPara: false,
-        secondPara: false
+        secondPara: false,
+        posts:[
+            {title: 'First post'},
+            {title: 'Second post'},
+            {title: 'Third post'},
+            {title: 'Fourth post'},
+            ],
              }">
 
-        <h1 x-text="message"></h1>
-        <p x-show="firstPara" x-transition>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
-            text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-            release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-        <p x-show="secondPara">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
-            text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-            release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
+        <div>
+            <h1 x-text="message"></h1>
+            <p x-show="firstPara" x-transition>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not
+                only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
+                release of Letraset sheets containing Lorem Ipsum passages,
+                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+            <p x-show="secondPara">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not
+                only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
+                release of Letraset sheets containing Lorem Ipsum passages,
+                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
 
-        <button x-on:click="firstPara = !firstPara ,secondPara=!secondPara">Toggle</button>
+            <button x-on:click="firstPara = !firstPara ,secondPara=!secondPara">Toggle</button>
 
-        <h2 x-text="name">Dynamic Data : </h2>
+            <h2 x-text="name">Dynamic Data : </h2>
 
-        <div x-effect="console.log(input_data)"></div>
+            <div x-effect="console.log(firstPara)"></div>
 
-        <input type="text" x-model="input_data" :placeholder="place_data">
-        <p x-text="input_data"></p>
+            <input type="text" x-model="input_data" :placeholder="place_data">
+            <p x-text="input_data"></p>
+        </div>
+
+        <template x-if="firstPara">
+            <p style="color: red;">If Conditio in Alpine.js is working</p>
+        </template>
+
+        <hr>
+
+        <h2>For loop in apline.js</h2>
+        <template x-for="post in posts">
+            <div x-text="post.title"></div>
+        </template>
+
+        <hr>
+
+        <div>
+            <!--   ref and refs -->
+            <div x-ref="text"></div>
+            <button @click="$refs.text.innerText = 'Hello World'">
+                Click
+            </button>
+        </div>
+
     </div>
+    <script>
+        document.addEventListener('alpine:init', () => {
+            Apline.store('darkMode', {
+                on: false,
+                toggle() {
+                    this.on = !this.on
+                }
+            })
+        })
+    </script>
 
 </body>
 
